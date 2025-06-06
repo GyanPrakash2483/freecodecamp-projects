508fbc90ad47313a51d6da0d04a4d286fc9988d2
GyanPrakash2483 <gyanprakash2483@gmail.com>
2025-06-06 16:33:44 +0530

    Timestamp Microservice Project

---
+++ boilerplate-project-timestamp/index.js
@@ -0,0 +1,60 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api', (req, res) => {
+  
+  const date = new Date(Date.now())
+
+  res.json({
+    unix: date.valueOf(),
+    utc: date.toUTCString()
+  })
+})
+
+app.get('/api/:date', (req, res) => {
+  let date = new Date(req.params.date)
+
+  if(!date.valueOf()) {
+    date = new Date(Number(req.params.date))
+  }
+
+  if(!date.valueOf()) {
+    res.json({
+      error: "Invalid Date"
+    })
+    return
+  }
+
+  res.json({
+    unix: date.valueOf(),
+    utc: date.toUTCString()
+  })
+})
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
